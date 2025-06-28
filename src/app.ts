19268fd5d74d492c57d10605c471d120545b4de6
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-06-28 14:31:15 +0600

    Notfound api routing setup done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import globalErrorHandeling from './app/middlewares/globalErrorHandeling';
+import notFound from './app/middlewares/notFound';
 const app = express();
 
 const testServer = async (req: Request, res: Response) => {
@@ -10,5 +11,7 @@ app.get('/', testServer);
 
 // global error handeler
 app.use(globalErrorHandeling);
+// not found router handeler
+app.use(notFound);
 
 export default app;
---
+++ src/app/middlewares/notFound.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.OK).json({
+    success: false,
+    message: 'SORRY API NOT FOUND!!',
+    error: '',
+  });
+  return;
+};
+
+export default notFound;
